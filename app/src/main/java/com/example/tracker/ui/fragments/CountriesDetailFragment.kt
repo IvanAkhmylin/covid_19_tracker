5b32b732f6eb76dde11eff576a971409c0db6503
vanganistan <vanganistan@gmail.com>
2020-04-20 23:36:52 +0300

    Data Update
    data class updated
    ui changed with new data

--- app/src/main/java/com/example/tracker/ui/fragments/CountriesDetailFragment.kt
+++ app/src/main/java/com/example/tracker/ui/fragments/CountriesDetailFragment.kt
@@ -13,6 +13,9 @@ import androidx.fragment.app.Fragment
 import com.example.tracker.R
 import com.example.tracker.model.CountriesStatisticModel
 import com.facebook.drawee.view.SimpleDraweeView
+import kotlinx.android.synthetic.main.overall_statistic_layout.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CountriesDetailFragment(dataModel: CountriesStatisticModel?) : Fragment() {
     private var data = dataModel
@@ -28,46 +31,33 @@ class CountriesDetailFragment(dataModel: CountriesStatisticModel?) : Fragment() 
 
     @SuppressLint("SetTextI18n", "ResourceAsColor")
     private fun init(v: View) {
-        data?.countryInfo?.flag?.let {
+
+        data.apply {
+            val dateFormat = SimpleDateFormat("dd MMMM yyyy hh:mm:ss a")
+            val netDate = Date(this!!.updated)
+
             v.findViewById<SimpleDraweeView>(R.id.country_flag).setImageURI(data?.countryInfo?.flag)
-        }
-        data?.country?.let {
-            v.findViewById<TextView>(R.id.country_name).text = data?.country
-        }
-        data?.countryInfo?.iso2?.let {
-            v.findViewById<TextView>(R.id.country_iso).text = "(${data?.countryInfo?.iso2}, ${data?.countryInfo?.iso3})"
-        }
-        data?.cases?.let {
+            v.findViewById<TextView>(R.id.last_update).text = "Updated: ${dateFormat.format(netDate)}"
+            v.findViewById<TextView>(R.id.country_name).text = "Country: ${data?.country}"
+            v.findViewById<TextView>(R.id.country_iso).text = "Country Code: ${data?.countryInfo?.iso2}, ${data?.countryInfo?.iso3}"
+            v.findViewById<TextView>(R.id.continent).text = "Continent ${data?.continent}"
+
             v.findViewById<TextView>(R.id.cases).text = data?.cases.toString()
-        }
-        data?.active?.let {
             v.findViewById<TextView>(R.id.active).text = data?.active.toString()
-        }
-        data?.critical?.let {
             v.findViewById<TextView>(R.id.critical).text = data?.critical.toString()
-        }
-        data?.todayCases?.let {
             v.findViewById<TextView>(R.id.today_cases).text = data?.todayCases.toString()
-        }
-        data?.deaths?.let {
             v.findViewById<TextView>(R.id.deaths).text = data?.deaths.toString()
-        }
-        data?.todayDeaths?.let {
             v.findViewById<TextView>(R.id.today_deaths).text = data?.todayDeaths.toString()
-        }
-        data?.recovered?.let {
             v.findViewById<TextView>(R.id.recovered).text = data?.recovered.toString()
+            v.findViewById<TextView>(R.id.tests).text = data?.tests.toString()
+            v.findViewById<TextView>(R.id.cases_per_million).text = data?.casesPerOneMillion.toString()
+            v.findViewById<TextView>(R.id.deaths_per_million).text = data?.deathsPerOneMillion.toString()
+            v.findViewById<TextView>(R.id.tests_per_million).text = data?.testsPerOneMillion.toString()
+
         }
 
-        data?.tests?.let {
-            v.findViewById<TextView>(R.id.tests).text = data?.tests.toString()
-        }
-        data?.casesPerOneMillion?.let {
-            v.findViewById<TextView>(R.id.cases_per_million).text = data?.casesPerOneMillion.toString()
-        }
-        data?.deathsPerOneMillion?.let {
-            v.findViewById<TextView>(R.id.deaths_per_million).text = data?.deathsPerOneMillion.toString()
-        }
+
+
 
     }
 
